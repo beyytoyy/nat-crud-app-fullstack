6449b56615acb8627aec24c1cb192a35dd726eb3
beyytoyy <lunox24722@gmail.com>
2026-06-02 21:01:39 +0800

    Fixes: change environments based on the railway

--- nat-backend/routes/nat.js
+++ nat-backend/routes/nat.js
@@ -3,13 +3,16 @@
   const mysql = require("mysql2");
 
   const db = mysql.createPool({
-      host: process.env.DB_HOST || "localhost",
-      user: process.env.DB_USER || "root",
-      password: process.env.DB_PASSWORD || "",
-      database: process.env.DB_NAME || "natdb",
-      port: process.env.DB_PORT || 3306,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
       waitForConnections: true,
       connectionLimit: 10,
+      ssl: {
+        rejectUnauthorized: false,
+      },
   });
 
   // TEST CONNECTION
--- nat-backend/server.js
+++ nat-backend/server.js
@@ -11,7 +11,23 @@ const PORT = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
+
 // ROUTES
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'NAT CRUD API is running',
+        endpoints: {
+            health: '/health',
+            getAllrecords: '/api/nat',
+            getSingleRecord: '/api/nat/:id',
+            createRecord: 'POST /api/nat',
+            updateRecord: 'PUT /api/nat/:id',
+            deleteRecord: 'DELETE /api/nat/:id'
+        }
+    });
+});
+
 app.use('/api/nat', natRoutes);
 
 // HEALTH CHECK
